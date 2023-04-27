e6eec8039e254d1be99f58b398e2b51189c754bb
Adebobola01 <m.oyedunmade@gmail.com>
2023-04-27 11:48:08 +0100

    feat: added the select btn

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -5,6 +5,7 @@ import NavigationItem from "../Navigation/NavigationItem";
 import ThreeDots from "../Buttons/3dots";
 import ConnectBtn from "../Buttons/connectBtn";
 import Search from "../Inputs/search";
+import ethImg from "../../assets/images/ethLogo.png"
 const Navbar = (prop: any) => {
     
     return (
@@ -15,11 +16,14 @@ const Navbar = (prop: any) => {
                 <NavigationItem link='/tokens' >Tokens</NavigationItem>
                 <NavigationItem link='/nfts'>NFTs</NavigationItem>
                 <NavigationItem link='/pools'>Pools</NavigationItem>
-
                 <ThreeDots/>
             </nav>
             <Search/>
-            <div>
+            <div className="nav-btns" >
+                <div className="nav-btns__select" >
+                    <img src={ethImg} alt="eth_logo" />
+                    <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.97168 1L6.20532 6L11.439 1" stroke="white"></path></svg>
+                </div>
                 <ConnectBtn/>
             </div>
         </div>
