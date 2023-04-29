2b7a4a8312fdfb87cb077b20cd2924a9569c7ed6
Adebobola01 <m.oyedunmade@gmail.com>
2023-04-29 14:47:59 +0100

    fix: search functionality

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -6,6 +6,7 @@ import ThreeDots from "../Buttons/3dots";
 import ConnectBtn from "../Buttons/connectBtn";
 import Search from "../Inputs/search";
 import ethImg from "../../assets/images/ethLogo.png"
+import magnifierImg from "../../assets/images/search.svg"
 const Navbar = (prop: any) => {
     
     return (
@@ -20,6 +21,9 @@ const Navbar = (prop: any) => {
             </nav>
             <Search/>
             <div className="nav-btns" >
+                <div className="nav-btns__search">
+                    <img src={magnifierImg} alt="mag_img" />
+                </div>
                 <div className="nav-btns__select" >
                     <img src={ethImg} alt="eth_logo" />
                     <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.97168 1L6.20532 6L11.439 1" stroke="white"></path></svg>
