07ccaf9cc19aeacf1d23c36c51524de02809d05e
Adebobola01 <m.oyedunmade@gmail.com>
2023-06-03 20:26:09 +0100

    fix: hero

--- src/Components/Swap/index.tsx
+++ src/Components/Swap/index.tsx
@@ -6,23 +6,25 @@ import "./swap.scss";
 const Swap = (props: any) => {
     
     return (
-        <div className={`swap_back ${props.hero ? "hero-swap" : null}`} >
-            <div className={`swap-container`}>
-                <div className="swap-header" >
-                    <div>
-                        <span>Swap</span>
-                        <span>Buy</span>
+        <div className={`swap_back_container`} >
+            <div className={`swap_back ${props.hero ? "hero-swap" : null}`} >
+                <div className={`swap-container`}>
+                    <div className="swap-header" >
+                        <div>
+                            <span>Swap</span>
+                            <span>Buy</span>
+                        </div>
+                        <Settings/>
                     </div>
-                    <Settings/>
-                </div>
-                <div className="swap-inputs">
-                    <SwapInput token="from" click={props.click} />
-                    <SwapInput token="to" click={props.click} />
-                    <div className="swap-switch">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#5D6785" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>
+                    <div className="swap-inputs">
+                        <SwapInput token="from" click={props.click} />
+                        <SwapInput token="to" click={props.click} />
+                        <div className="swap-switch">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#5D6785" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>
+                        </div>
                     </div>
+                    <button className="swap-btn">Connect Wallet</button>
                 </div>
-                <button className="swap-btn">Connect Wallet</button>
             </div>
         </div>
     )
